b3166333c8d2fa52335de1ec409615a05f8570cd
祁梦楠 <1710121104@vip.henu.edu.cn>
2019-04-27 17:47:47 +0800

    添加日历，添加分享和设置的fragment

--- app/src/main/java/com/example/meet/activity/MainActivity.java
+++ app/src/main/java/com/example/meet/activity/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.meet.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.meet.R;
+import com.example.meet.fragment.SettingFragment;
+import com.example.meet.fragment.ShareFragment;
+import com.example.meet.helper.NavHelper;
+import com.example.meet.fragment.TaskFragment;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        CalendarView.OnYearChangeListener,//宿主Activity实现接口，在fragment中实例化
+        CalendarView.OnCalendarSelectListener,
+        NavHelper.OnTabChangeListener
+         {
 
-    private TextView mTextMessage;
+    private BottomNavigationView navigationView;
+    private FragmentManager mFragmentManager;
+    private NavHelper<Integer> mNavHelper;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,27 +36,75 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_checkItem:
-                    mTextMessage.setText(R.string.title_home);
+                    mNavHelper.performClickMenu(R.id.navigation_checkItem);
                     return true;
                 case R.id.navigation_share:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mNavHelper.performClickMenu(R.id.navigation_share);
                     return true;
                 case R.id.navigation_set:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mNavHelper.performClickMenu(R.id.navigation_set);
                     return true;
             }
             return false;
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //隐藏系统标题栏
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        initView();
+
     }
 
-}
+    private void initView() {
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        mFragmentManager = getSupportFragmentManager();
+
+        mNavHelper = new NavHelper<>(this,R.id.lay_container,mFragmentManager,this);
+        mNavHelper.add(R.id.navigation_checkItem,new NavHelper.Tab<Integer>(TaskFragment.class,R.id.navigation_checkItem))
+                .add(R.id.navigation_share,new NavHelper.Tab<Integer>(ShareFragment.class,R.id.navigation_share))
+                .add(R.id.navigation_set,new NavHelper.Tab<Integer>(SettingFragment.class,R.id.navigation_set));
+
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Menu menu = navigationView.getMenu();
+        //navigation默认选中任务界面
+        navigationView.setSelectedItemId(menu.getItem(1).getItemId());
+        //触发首次选中TaskFragment
+        menu.performIdentifierAction(R.id.navigation_checkItem,0);
+
+    }
+
+
+
+
+
+    @Override
+    public void onYearChange(int year) {
+
+    }
+
+
+
+     //fragment切换时设置
+    @Override
+    public void onTabChange(NavHelper.Tab newTab, NavHelper.Tab oldTab) {
+
+    }
+
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) { }
+
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) { }
+         }
+
