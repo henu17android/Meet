dc86602030f451b8a13251dfb12b805ae195f269
gh704580137 <704580137@qq.com>
2019-05-03 19:32:33 +0800

    gh-Settings-521

---
+++ app/src/main/java/com/example/meet/activity/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.meet.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.meet.R;
+import com.example.meet.fragment.SettingFragment;
+import com.example.meet.fragment.ShareFragment;
+import com.example.meet.helper.NavHelper;
+import com.example.meet.fragment.TaskFragment;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarView;
+
+public class MainActivity extends AppCompatActivity implements
+        CalendarView.OnYearChangeListener,//宿主Activity实现接口，在fragment中实例化
+        CalendarView.OnCalendarSelectListener,
+        NavHelper.OnTabChangeListener
+         {
+
+    private BottomNavigationView navigationView;
+    private FragmentManager mFragmentManager;
+    private NavHelper<Integer> mNavHelper;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_checkItem:
+                    mNavHelper.performClickMenu(R.id.navigation_checkItem);
+                    return true;
+                case R.id.navigation_share:
+                    mNavHelper.performClickMenu(R.id.navigation_share);
+                    return true;
+                case R.id.navigation_set:
+                    mNavHelper.performClickMenu(R.id.navigation_set);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //隐藏系统标题栏
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        initView();
+
+    }
+
+    private void initView() {
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        mFragmentManager = getSupportFragmentManager();
+
+        mNavHelper = new NavHelper<>(this,R.id.lay_container,mFragmentManager,this);
+        mNavHelper.add(R.id.navigation_checkItem,new NavHelper.Tab<Integer>(TaskFragment.class,R.id.navigation_checkItem))
+                .add(R.id.navigation_share,new NavHelper.Tab<Integer>(ShareFragment.class,R.id.navigation_share))
+                .add(R.id.navigation_set,new NavHelper.Tab<Integer>(SettingFragment.class,R.id.navigation_set));
+
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Menu menu = navigationView.getMenu();
+        //navigation默认选中任务界面
+        navigationView.setSelectedItemId(menu.getItem(1).getItemId());
+        //触发首次选中TaskFragment
+        menu.performIdentifierAction(R.id.navigation_checkItem,0);
+
+    }
+
+
+
+
+
+    @Override
+    public void onYearChange(int year) {
+
+    }
+
+
+
+     //fragment切换时设置
+    @Override
+    public void onTabChange(NavHelper.Tab newTab, NavHelper.Tab oldTab) {
+
+    }
+
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) { }
+
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) { }
+         }
+
---
+++ app/src/main/java/com/example/meet/calenderView/MyWeekView.java
@@ -0,0 +1,98 @@
+package com.example.meet.calenderView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.WeekView;
+
+/**
+ * 周视图
+ */
+public class MyWeekView extends WeekView {
+
+    private Paint mProgressPaint = new Paint();
+    private Paint mNoneProgressPaint = new Paint();
+    private int mRadius;
+
+    public MyWeekView(Context context) {
+        super(context);
+        mProgressPaint.setAntiAlias(true);
+        mProgressPaint.setStyle(Paint.Style.STROKE);
+        mProgressPaint.setStrokeWidth(dipToPx(context,2.2f));
+        mProgressPaint.setColor(0xBB8A2BE2);
+
+        mNoneProgressPaint.setAntiAlias(true);
+        mNoneProgressPaint.setStyle(Paint.Style.STROKE);
+        mNoneProgressPaint.setStrokeWidth(dipToPx(context,2.2f));
+        mNoneProgressPaint.setColor(0x90CfCfCf);
+
+    }
+
+    @Override
+    protected void onPreviewHook() {
+        mRadius = Math.min(mItemHeight,mItemWidth)/11 * 4 ;
+    }
+
+    @Override
+    protected boolean onDrawSelected(Canvas canvas, Calendar calendar, int x, boolean hasScheme) {
+        int cx = x + mItemWidth /2;
+        int cy = mItemHeight / 2;
+        canvas.drawCircle(cx,cy,mRadius,mSelectedPaint);
+        return false;
+    }
+
+    @Override
+    protected void onDrawScheme(Canvas canvas, Calendar calendar, int x) {
+       int cx = x +mItemWidth /2 ;
+       int cy = mItemHeight /2;
+
+       int angle = getAngle(Integer.parseInt(calendar.getScheme()));
+
+        RectF progressRectF = new RectF(cx-mRadius,cy-mRadius,cx+mRadius,cy+mRadius);
+        canvas.drawArc(progressRectF, -90,angle,false,mProgressPaint);
+
+        RectF noneRectF = new RectF(cx-mRadius,cy-mRadius,cx+mRadius,cy+mRadius);
+        canvas.drawArc(noneRectF,angle-90,360-angle,false,mNoneProgressPaint);
+
+    }
+
+    @Override
+    protected void onDrawText(Canvas canvas, Calendar calendar, int x, boolean hasScheme, boolean isSelected) {
+        float baselineY = mTextBaseLine;
+        int cx = x + mItemWidth / 2;
+
+        if (isSelected) {
+            canvas.drawText(String.valueOf(calendar.getDay()),
+                    cx,
+                    baselineY,
+                    mSelectTextPaint);
+        } else if (hasScheme) {
+            canvas.drawText(String.valueOf(calendar.getDay()),
+                    cx,
+                    baselineY,
+                    calendar.isCurrentDay() ? mCurDayTextPaint :
+                            calendar.isCurrentMonth() ? mSchemeTextPaint : mOtherMonthTextPaint);
+
+        } else {
+            canvas.drawText(String.valueOf(calendar.getDay()), cx, baselineY,
+                    calendar.isCurrentDay() ? mCurDayTextPaint :
+                            calendar.isCurrentMonth() ? mCurMonthTextPaint : mOtherMonthTextPaint);
+        }
+    }
+
+    private static int getAngle(int progress){
+        return  (int)(progress*3.6);
+    }
+
+
+    /*
+   dp转px
+    */
+    private static int dipToPx(Context context,float dpValue){
+        final float scale =  context.getResources().getDisplayMetrics().density;
+        return (int)  (dpValue * scale+0.5f);
+    }
+}
